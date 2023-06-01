b4c49716a7a11f1016d9ec7cb620e775c96b7d54
tuhinbiswas24 <120563029+tuhinbiswas24@users.noreply.github.com>
2023-06-01 19:07:13 +0530

    Create Primality Test.cpp

---
+++ C++ Solutions/Primality Test.cpp
@@ -0,0 +1,19 @@
+class Solution{
+    public:
+        bool isPrime(int N)
+    {
+        //Your code here
+        if(N<=1)
+            return false;
+        if(N==2 || N==3)
+            return true;
+        if((N%2==0) || (N%3==0))
+            return false;
+        for(int i=5;i*i<=N;i+=6){
+            if((N%i)==0 || (N%(i+2)==0))
+                return false;
+        }    
+        return true;
+    }
+
+};
