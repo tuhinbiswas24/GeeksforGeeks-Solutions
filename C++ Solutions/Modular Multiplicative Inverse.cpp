0419b74b81ea286eca0512df626c95f5c9dcd05d
tuhinbiswas24 <120563029+tuhinbiswas24@users.noreply.github.com>
2023-06-01 19:35:31 +0530

    Create Modular Multiplicative Inverse.cpp

---
+++ C++ Solutions/Modular Multiplicative Inverse.cpp
@@ -0,0 +1,13 @@
+class Solution{
+    public:
+    //Complete this function
+    int modInverse(int a, int m)
+    {
+        //Your code here
+        for(int i=0;i<m;i++){
+            if((a*i)%m==1)
+                return i;
+        }
+        return -1;
+    }
+};
