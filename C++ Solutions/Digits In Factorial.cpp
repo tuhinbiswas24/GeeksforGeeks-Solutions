91497bd4d27c8026bf814c53e5b4485a1a10f5e6
tuhinbiswas24 <120563029+tuhinbiswas24@users.noreply.github.com>
2023-06-01 19:05:53 +0530

    Update Digits In Factorial.cpp

--- C++ Solutions/Digits In Factorial.cpp
+++ C++ Solutions/Digits In Factorial.cpp
@@ -1,1 +1,12 @@
-
+class Solution{
+    public:
+    int digitsInFactorial(int N)
+    {
+        // code here
+        double fact=log10(1);
+        for(int i=1;i<=N;i++){
+            fact+=log10(i);
+        }
+        return floor(fact + 1);
+    }
+};
