a1f2370d049cf4f185e0a1a57ff17b99ec31c957
tuhinbiswas24 <120563029+tuhinbiswas24@users.noreply.github.com>
2023-06-01 19:47:29 +0530

    Create Count Total Set Bits.cpp

---
+++ C++ Solutions/Count Total Set Bits.cpp
@@ -0,0 +1,12 @@
+class Solution{
+    public:
+    // n: input to count the number of set bits
+    //Function to return sum of count of set bits in the integers from 1 to n.
+    int countSetBits(int n)
+    {
+        if(n==0)
+            return 0;
+        int x=log2(n);
+        return (1<<(x-1))*x + (n-(1<<x) + 1) + countSetBits(n-(1<<x));
+    }
+};
